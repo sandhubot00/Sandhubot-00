a772311c7f8a21732b35d3ee44f02d76387d07cf
akmoviebot <mrak7753@gmail.com>
2024-11-23 13:48:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Akmoviefile:Akmoviefile@cluster0.xwasq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "Sharma Movies")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Akmovie')
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
